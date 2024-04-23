f41896023d45f64107a47b90caf967953ef00891
Dezert <68746157+Dezert01@users.noreply.github.com>
2024-04-23 10:50:44 +0200

    Sign in

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,82 @@
+import { useRef, useState } from "react";
+import { Parcel } from "./classes/parcel";
+import { User } from "./classes/user";
+import { MockUsers } from "./mock-data/users";
+
 function App() {
-  return <>asd</>;
+  const [parcels, setParcels] = useState<Parcel[]>([]);
+  const [currentUser, setCurrentUser] = useState<User | undefined>();
+  const [users, setUsers] = useState<User[]>(MockUsers);
+  const [isSigningUp, setIsSigningUp] = useState<boolean>(false);
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const phoneRef = useRef<HTMLInputElement>(null);
+
+  const handleSigning = () => {
+    if (
+      !usernameRef ||
+      !usernameRef.current ||
+      !passwordRef.current ||
+      !passwordRef.current
+    ) {
+      alert("Something went wrong");
+      return;
+    }
+    if (isSigningUp) {
+      // New Account
+      if (!phoneRef || !phoneRef.current) return;
+      let user = users.find((el) => el.name === usernameRef.current?.value);
+    } else {
+      // Login In
+      const user = users.find((el) => el.name === usernameRef.current?.value);
+      const confirmPassword = user?.password === passwordRef.current?.value;
+      if (user && confirmPassword) {
+        setCurrentUser(user);
+        alert(`Sucessful sign in as ${user.name}`);
+      } else {
+        alert("Wrong username or password");
+      }
+
+      console.log(user);
+    }
+  };
+
+  return (
+    <div className="flex h-full w-full flex-wrap gap-2">
+      {currentUser ? (
+        <div className="card">
+          <h1>Currently logged in as:</h1>
+          <h2 className="font-bold">{currentUser.name}</h2>
+          <button className="button" onClick={() => setCurrentUser(undefined)}>
+            Sign out
+          </button>
+        </div>
+      ) : (
+        <div className="card">
+          <h1>{isSigningUp ? "Sign Up" : "Sign In"}</h1>
+          <input placeholder="Username" type="login" ref={usernameRef} />
+          <input placeholder="password" type="password" ref={passwordRef} />
+          {isSigningUp ? (
+            <input placeholder="phone number" type="tel" ref={phoneRef} />
+          ) : null}
+          <button
+            onClick={() => setIsSigningUp((prev) => !prev)}
+            className="mb-2 text-xs text-blue-500 hover:underline"
+          >
+            {isSigningUp
+              ? "Sign in using existing account"
+              : "Create new account"}
+          </button>
+          <button onClick={handleSigning} className="button">
+            {isSigningUp ? "Sign Up" : "Sign In"}
+          </button>
+        </div>
+      )}
+      <div className="card">
+        <h1>Nadaj Paczkę</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/mock-data/users.ts
@@ -0,0 +1,6 @@
+import { User } from "../classes/user";
+
+export const MockUsers: User[] = [
+  new User("admin", 1, "admin", 123123),
+  new User("kuba", 2, "password", 33221),
+];
