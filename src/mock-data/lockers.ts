1fddd66150f6e461f9f2fb30c9c7faa3cb7f56d9
Dezert <68746157+Dezert01@users.noreply.github.com>
2024-04-24 14:22:51 +0200

    update mock data

--- src/mock-data/lockers.ts
+++ src/mock-data/lockers.ts
@@ -2,7 +2,7 @@ import { Locker } from "../classes/locker";
 import { mockSlots } from "./slots";
 
 export const mockLockers: Locker[] = [
-  new Locker(1, "Address 1", mockSlots, []),
-  new Locker(2, "Address 2", mockSlots, []),
-  new Locker(3, "Address 3", mockSlots, []),
+  new Locker(1, "Address 1", mockSlots[0], []),
+  new Locker(2, "Address 2", mockSlots[1], []),
+  new Locker(3, "Address 3", mockSlots[2], []),
 ];
--- src/mock-data/slots.ts
+++ src/mock-data/slots.ts
@@ -1,8 +1,20 @@
 import { Slot } from "@/classes/slot";
 import { ParcelSize } from "@/enums/ParcelSize";
 
-export const mockSlots: Slot[] = [
-  new Slot(1, ParcelSize.SMALL, null),
-  new Slot(2, ParcelSize.MEDIUM, null),
-  new Slot(3, ParcelSize.LARGE, null),
+export const mockSlots: Slot[][] = [
+  [
+    new Slot(1, ParcelSize.SMALL, null),
+    new Slot(2, ParcelSize.MEDIUM, null),
+    new Slot(3, ParcelSize.LARGE, null),
+  ],
+  [
+    new Slot(4, ParcelSize.SMALL, null),
+    new Slot(5, ParcelSize.MEDIUM, null),
+    new Slot(6, ParcelSize.LARGE, null),
+  ],
+  [
+    new Slot(7, ParcelSize.SMALL, null),
+    new Slot(8, ParcelSize.MEDIUM, null),
+    new Slot(9, ParcelSize.LARGE, null),
+  ],
 ];
